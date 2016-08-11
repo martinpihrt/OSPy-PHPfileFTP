1a94c2f8524cf9f78600a1c933ee49f6ff976544
Martin Pihrt <martinpihrt@seznam.cz>
2016-08-11 21:38:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -69,11 +69,14 @@ if(isset($_POST['tlacitko']))
         if ($press!=""){
           echo "<br><b>Tlak vody (&#269;idlo):</b> ";  if ($press=='1'){ echo ("<font color='Red'>bez tlaku</font>"); } else { echo ("<font color='Green'>v po&#345;&aacute;dku</font>"); }}
         if ($tank!=""){
-          echo "<br><b>N&aacute;dr&#382; s vodou:</b> " . $tank . " (cm)<br>"; } 
-        echo "<br><b>D&eacute;&#353;&#357;ov&eacute; &#269;idlo: </b>";  if ($rain=='1'){ echo ("<font color='Red'>pr&#353;&iacute;</font>"); } else { echo ("<font color='Green'>bez de&#353;t&#283;</font>"); }
+          echo "<br><b>N&aacute;dr&#382; s vodou:</b> " . $tank . " (cm)"; } 
+        if ($raindel!=""){
+          echo "<br><b>D&eacute;&#353;&#357;ov&eacute; zpo&#382;den&iacute;:</b><font color='Red'> " . $raindel . "</font>"; } 
+        echo "<br><b>D&eacute;&#353;&#357;ov&eacute; &#269;idlo: </b>";  if ($rain=='1'){ echo ("<font color='Red'>pr&#353;&iacute;</font><br>"); } else { echo ("<font color='Green'>bez de&#353;t&#283;</font><br>"); }
         if ($lastrun!=""){
-          echo "<br><b>Naposledy b&#283;&#382;el:</b> " . $lastrun . "<br><br>"; } 
+          echo "<br><b>Naposledy b&#283;&#382;el:</b> " . $lastrun . "<br><br>"; } else { echo "<br><br>"; }
         if ($system=='0'){ echo ("<button name=\"tlacitko\" value=\"stop\"><h3><font color='Red'>Zastavit syst&eacute;m</font></h3></button>"); }  else { echo ("<button name=\"tlacitko\" value=\"start\"><h3>Spustit syst&eacute;m</h3></button>"); }   
+  
         echo "<b>&nbsp; Syst&eacute;m:</b> ";  if ($system=='0'){ echo ("povolen"); } else { echo ("zastaven"); }
         echo "&nbsp;";
         if ($schedul=='1'){ echo ("<button name=\"tlacitko\" value=\"manu\"><h3>P&#345;epnout na ru&#269;n&iacute;</h3></button>"); } else { echo ("<button name=\"tlacitko\" value=\"auto\"><h3>P&#345;epnout na pl&aacute;nova&#269;</h3></button>"); }
@@ -100,6 +103,9 @@ if(isset($_POST['tlacitko']))
     echo "</legend>";
     if ($state[$i-1]=='1'){ echo ("<button name=\"tlacitko\" value=\"V$i\"><h3>Vypnout</h3></button>"); } else { echo ("<button name=\"tlacitko\" value=\"Z$i\"><h3>Zapnout</h3></button>"); }  
     echo "&nbsp;<b>Stav:</b> "; if ($state[$i-1]=='1') { echo ("ZAPNUTO"); } else { echo ("VYPNUTO"); } 
+    if ($masterstat!=''){
+      if ($masterstat==$i-1){ echo "&nbsp;<font color='Gray'>(hlavn&iacute; stanice)</font>";} 
+      }
     echo "</fieldset>";
     }
 ?>
